27a1689e5711ca1a2fdf540bfe5366fe2fa7cbe6
TenLinks20 <uzonduberto94@gmail.com>
2025-04-27 17:43:01 +0200

    added functions to split text with image and link formatting

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 
     
@@ -23,5 +24,77 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
 
     return new_nodes
 
+def extract_markdown_images(text: str) -> list[tuple]:
+    extracted_text = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return extracted_text
+
+def extract_markdown_links(text: str) -> list[tuple]:
+    extracted_text = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return extracted_text
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+
+        text_to_process = node.text 
+        images = extract_markdown_images(text_to_process)
+
+        if not images:
+            new_nodes.append(node)
+            continue
+
+        for img_alt, img_url in images:
+            delimiter = f"![{img_alt}]({img_url})"
+            parts = text_to_process.split(delimiter, 1)
+
+            if len(parts) != 2:
+                raise ValueError(f"Invalid markdown: unmatched image delimiter '{delimiter}'")
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.NORMAL))
+
+            new_nodes.append(TextNode(img_alt, TextType.IMAGE, img_url))
+            text_to_process = parts[1]
+
+        if text_to_process:
+            new_nodes.append(TextNode(text_to_process, TextType.NORMAL))
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+            continue
+
+        text_to_process = node.text
+        links = extract_markdown_links(text_to_process)
+
+        if not links:
+            new_nodes.append(node)
+            continue
+
+        for link_text, link_url in links:
+            delimiter = f"[{link_text}]({link_url})"
+            parts = text_to_process.split(delimiter, 1)
+
+            if len(parts) != 2:
+                raise ValueError(f"Invalid markdown: unmatched link delimiter '{delimiter}'")
+
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.NORMAL))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            text_to_process = parts[1]
+
+        if text_to_process:
+            new_nodes.append(TextNode(text_to_process, TextType.NORMAL))
+
+    return new_nodes
 
     
