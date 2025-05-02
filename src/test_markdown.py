3ca12d8d9285288a0a2258e4855cb4cd7b1a149f
TenLinks20 <uzonduberto94@gmail.com>
2025-05-02 15:48:10 +0200

    added raw text to text node functionality

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -4,7 +4,8 @@ from markdown import (
     extract_markdown_links,
     extract_markdown_images,
     split_nodes_image,
-    split_nodes_link
+    split_nodes_link,
+    text_to_textnodes
 )
 
 from textnode import TextNode, TextType
@@ -168,6 +169,26 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", TextType.NORMAL),
             ],
             new_nodes,
+        )
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.NORMAL),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.NORMAL),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.NORMAL),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.NORMAL),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
         )
 
 
